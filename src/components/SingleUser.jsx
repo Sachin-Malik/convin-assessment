d7ea402493aa841d969d2c8a657d2852520d1d8f
Sachin Malik <sachinmalik199821@gmail.com>
2022-03-12 13:17:17 +0530

    Add Deploy

--- src/components/SingleUser.jsx
+++ src/components/SingleUser.jsx
@@ -5,10 +5,10 @@ function SingleUser({data}) {
     <div>
       <h2 className='nav-header ml-2'>User Detail</h2>
       <div className='row justify-content-around mb-5'>
-         <div className='col-3 card shadow rounded p-3 user'>
+         <div className='col-md-3 col-12 card shadow rounded p-3 user'>
             <img src={data.avatar} alt='Profile Pic for User' />
          </div>
-         <div className='col-8 text-left p-5 card shadow rounded p-3 user-detail user-card-dashboard'>
+         <div className='col-md-8 col-12 text-left p-5 card shadow rounded p-3 user-detail user-card-dashboard'>
            <h2>Name: {data.first_name} {data.last_name}</h2>
            <hr />
            <h2>Email Id: {data.email}</h2>
